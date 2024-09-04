a30effefdbaea826cccc58393ab3d2d0927d335d
Michael Poltronieri Tang <mtang8264@gmail.com>
2024-09-04 11:28:15 -0400

    Created a new celestial body type called satellite which is can be used for moons and other small orbiting objects

---
+++ Godot_Project/Scripts/Celestial_Bodies/CB_Satellite.gd
@@ -0,0 +1,14 @@
+class_name CB_Satellite extends Celestial_Body
+
+const cb_type = CB_Type.SATELLITE
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if debugging_test_all_distances:
+		test_all_distances()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
